02ce5bef9e22d8d61e1abe7fa606091f2a5afba0
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2021-09-15 10:01:07 +0100

    Added JS to the header
    I managed to make JS work properly. Now if the user clicks on the hamburger it opens and if they click again it closes
    I added another function to check the width of the user’s screen and remove the small navbar if the screens is bigger

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 //Function to open the navbar on small screens when user clicks on the hamburger
 
-hamburger = document.getElementById('ul-header');
+const headerSmall = document.getElementById('header-small');
 
-function openNav() {
-  hamburger.style.display = 'block';
+function openCloseNav() {
+  if (headerSmall.style.display === "block") {
+    headerSmall.style.display = "none";
+  } else {
+    headerSmall.style.display = "block";
+  }
 }
+
+//Function to make the navbar disappear if the user changes to bigger screen even if it has been opened on the small one
+function removeNav(x) {
+  if (x.matches) {
+    headerSmall.style.display = "none";
+} else {
+  headerSmall.style.display = "none";
+}
+}
+
+var x = window.matchMedia("(max-width: 767px)")
+removeNav(x)
+x.addListener(removeNav)
+
