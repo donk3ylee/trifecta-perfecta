29c90c6c539147cf28b5c74d521714f07dee511f
Lee Whiting <82646243+donk3ylee@users.noreply.github.com>
2021-09-16 12:05:48 +0100

    updating myowl.js

--- assets/JS/myOwl.js
+++ assets/JS/myOwl.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-    navigation: true;
-    navigationText: false;
-    items: 3;
-  });
+    $(".owl-carousel").owlCarousel({
+
+
+      loop: true,
+          //Basic Speeds
+      slideSpeed : 200,
+      paginationSpeed : 800,
+
+      
+      //Autoplay
+      autoPlay : false,
+      goToFirst : true,
+      goToFirstSpeed : 1,
+    
+      // Navigation
+      navigation : true,
+      navigationText : false,  //["prev","next"],
+      pagination : false,
+      // paginationNumbers: true,
+    
+      // Responsive
+      responsive: true,
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [980,2],
+      itemsTablet: [768,2],
+      itemsMobile : [479,1]
+    });
+})
