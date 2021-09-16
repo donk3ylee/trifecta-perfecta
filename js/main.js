56a5dc3dedbcdec2f4385de0ad711505b31f691a
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2021-09-16 11:31:11 +0100

    About us page
    I added all the details in the about us page html file
    I added hero image using the template from the home page
    I added about us text field
    I added a photo of the bakery (which has to be discussed with the team)
    I added team members with photo, name and short info
    I added social media links
    I added map and streetview
    I worked on the responsiveness of the screen and added the needed properties for all screen sizes

--- js/main.js
+++ js/main.js
@@ -31,3 +31,20 @@ var x = window.matchMedia("(max-width: 767px)")
 removeNav(x)
 x.addListener(removeNav)
 
+
+//Google Maps API in About Us page
+// Initialize and add the map
+function initMap() {
+  // The location of the bakery
+  const bakeryLocation = { lat: 52.62145224678766, lng: 1.280472649413193 }; 
+  // The map, centered at the bakery
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: bakeryLocation,
+  });
+  // The marker, positioned at the bakery
+  const marker = new google.maps.Marker({
+    position: bakeryLocation,
+    map: map,
+  });
+}
